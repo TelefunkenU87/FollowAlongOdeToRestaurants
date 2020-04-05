9da9e9705d9188c6980a1ab117fef1256a94c290
telefunk <telewebb@gmail.com>
2020-04-05 13:22:28 -0500

    Completion of first module

---
+++ OdeToFoodCodeAlong.Core/CuisineType.cs
@@ -0,0 +1,10 @@
+﻿namespace OdeToFoodCodeAlong.Core
+{
+    public enum CuisineType
+    {
+        None,
+        Mexican,
+        Italian,
+        Indian
+    }
+}
---
+++ OdeToFoodCodeAlong.Data/IRestaurantData.cs
@@ -0,0 +1,33 @@
+﻿using System.Linq;
+using OdeToFoodCodeAlong.Core;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OdeToFoodCodeAlong.Data
+{
+    public interface IRestaurantData
+    {
+        IEnumerable<Restaurant> GetAll();
+    }
+
+    public class InMemoryRestaurantData : IRestaurantData
+    {
+        List<Restaurant> restaurants;
+        public InMemoryRestaurantData()
+        {
+            restaurants = new List<Restaurant>()
+            {
+                new Restaurant { Id = 1, Name = "Scott's Pizza", Location = "Maryland", Cuisine=CuisineType.Italian},
+                new Restaurant { Id = 2, Name = "Cinnamon Club", Location = "London", Cuisine=CuisineType.Italian},
+                new Restaurant { Id = 3, Name = "La Costa", Location = "California", Cuisine=CuisineType.Mexican}
+            };
+        }
+        public IEnumerable<Restaurant> GetAll()
+        {
+            return from r in restaurants
+                   orderby r.Name
+                   select r;
+        }
+    }
+}
