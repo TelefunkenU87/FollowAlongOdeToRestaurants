198effe636a12aa193b242d34db92fa6a626851a
telefunk <telewebb@gmail.com>
2020-04-12 10:51:10 -0500

    Finished module 4

---
+++ OdeToFoodCodeAlong.Data/OdeToFoodDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OdeToFoodCodeAlong.Core;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OdeToFoodCodeAlong.Data
+{
+    public class OdeToFoodDbContext : DbContext
+    {
+        public OdeToFoodDbContext(DbContextOptions<OdeToFoodDbContext> options) : base(options)
+        {
+
+        }
+        
+        public DbSet<Restaurant> Restaurants { get; set; }
+    }
+}
